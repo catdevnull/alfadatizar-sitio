8af6a0af1d503855665ed0b6e8fdc2510c75458d
Nulo <git@nulo.in>
2023-06-28 23:03:19 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,liquid}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#fff5e8",
+      orange: "#ff4e1f",
+      celeste: "#b9d7ea",
+    },
+  },
+  plugins: [],
+};
