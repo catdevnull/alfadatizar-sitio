27c55d0ec293380b4d589c12dd757a46a21b8715
mati <git@mati.lol>
2023-07-03 12:31:58 -0300

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,liquid,11ty.jsx}"],
   theme: {
-    extend: {},
-    colors: {
-      blanco: "#fff5e8",
-      naranja: "#ff4e1f",
-      celeste: "#b9d7ea",
-      amarillo: "#f9af39",
-      violeta: "#dda7dd",
+    extend: {
+      colors: {
+        blanco: "#fff5e8",
+        naranja: "#ff4e1f",
+        celeste: "#b9d7ea",
+        amarillo: "#f9af39",
+        violeta: "#dda7dd",
+      },
     },
   },
   plugins: [],
