7bd27b1baf225fc71466222a62dda19392e0ddee
mati <git@mati.lol>
2023-07-02 23:11:51 -0300

    inlinear css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,28 @@
+const postcssImport = require("postcss-import");
+const cssnano = require("cssnano");
+const tailwind = require("tailwindcss");
+const tailwindConfig = require("./tailwind.config");
+const { readFile } = require("fs/promises");
+const tailwindPlugin = tailwind(tailwindConfig);
+const postcss = require("postcss")([
+  postcssImport(),
+  tailwindPlugin,
+  ...(process.env.ELEVENTY_ENV === "production" ? [cssnano()] : []),
+]);
+
+/**
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ */
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
-    watch: ["_site/**/*.css"],
+    watch: ["src/**/*.css"],
   });
 
-  eleventyConfig.addPassthroughCopy("**.png");
+  eleventyConfig.addShortcode("tailwind", async () => {
+    const from = "src/tailwind.css";
+    return await postcss.process(await readFile(from, "utf-8"), { from });
+  });
+  eleventyConfig.addPassthroughCopy("src/**.png");
 
   return {
     dir: {
